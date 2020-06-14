e37ff40d62cdb298ccb4ca6090bfd3ba2d3fe1d2
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-14 17:17:24 +0300

    Fix sliders

--- src/blocks/components/products/products.js
+++ src/blocks/components/products/products.js
@@ -1,68 +1,32 @@
-import $ from 'jquery'
-import 'slick-carousel/slick/slick'
+import Swiper from 'swiper'
 
-const $slider = $('.slider')
-const $randomSlider = $('.random-slider')
-
-const slickSettings = {
-  arrows: true,
-  dots: false,
-  mobileFirst: true,
-  infinite: false,
-  slidesToShow: 3,
-  accessibility: false,
-  responsive: [
-    {
-      breakpoint: 320,
-      settings: {
-        arrows: false,
-        variableWidth: true,
-      }
+const swiperSettings = {
+  slidesPerView: 'auto',
+  freeMode: true,
+  spaceBetween: 20,
+  navigation: {
+    prevEl: '.swiper-button.swiper-button-prev',
+    nextEl: '.swiper-button.swiper-button-next',
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
     },
-    {
-      breakpoint: 576,
-      settings: {
-        arrows: false,
-        variableWidth: true,
-      }
+    1025: {
+      spaceBetween: 20,
+      freeMode: false,
     },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 3,
-      }
+    1200: {
+      freeMode: false,
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 4,
-      }
-    },
-  ]
+  },
 }
 
-function preventFlickering(event, slick) {
-  slick.$slider.closest('.products').fadeTo(200, 1)
+function enableSwiper() {
+  new Swiper('.slider', swiperSettings)
+  new Swiper('.random-slider', swiperSettings)
 }
 
-$slider.on('init', preventFlickering)
-$randomSlider.on('init', preventFlickering)
-
-$.fn.randomize = function (selector) {
-  let $elems = selector ? $(this).find(selector) : $(this).children(),
-    $parents = $elems.parent()
-
-  $parents.each(function () {
-    $(this).children(selector).sort(function (childA, childB) {
-      // * Prevent last slide from being reordered
-      if($(childB).index() !== $(this).children(selector).length - 1) {
-        return Math.round(Math.random()) - 0.5
-      }
-    }.bind(this)).detach().appendTo(this)
-  })
-
-  return this
-}
-
-$slider.slick(slickSettings)
-$randomSlider.randomize().slick(slickSettings)
+enableSwiper()
